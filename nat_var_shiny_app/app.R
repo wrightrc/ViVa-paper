510a7af3a229198365421f58e954fe2b3c0fafcf
Morgan Hamm <morganohamm@gmail.com>
2017-12-01 17:32:30 -0800

    Started working on detail text for the app sections. commented some of the app code.

--- nat_var_shiny_app/app.R
+++ nat_var_shiny_app/app.R
@@ -5,9 +5,9 @@ library(RColorBrewer)
 
 CSSCode <- tags$head(tags$style(HTML("
                                        .checkboxformat {
-                                     -webkit-column-width: 350px;
-                                     -moz-column-width: 350px;
-                                     column-width: 350px;
+                                          -webkit-column-width: 350px;
+                                          -moz-column-width: 350px;
+                                          column-width: 350px;
                                      }
                                      ")))
 
@@ -20,43 +20,54 @@ ui <- fluidPage(
   tabsetPanel(
 
     tabPanel("SNP Stats",
-        ## Tab 1
+        ## Tab 1 ###############################################################
       textAreaInput(inputId = "gene_ids", label = "Type a list of gene loci in the box below, separated by commas ",
                     width = 600, height = 75, value = "AT3G62980, AT4G03190, AT3G26810, AT1G12820, AT4G24390, AT5G49980, AT2G39940" ),
       actionButton(inputId="STATS_submit", label = "Submit"),
       tags$hr(),
       tags$br(),
-      h3("Selected Gene Information"),
+      tags$h3("Selected Gene Information"),
+      tags$h5("this table provides details on the gene(s) input above, including transcript IDs, and chromosome position information on the start and end of the transcript"),
       downloadButton("tab1.downloadGeneInfo","Download Content of Table Below"),
-      tableOutput("table"),
+      tableOutput("tab1.genes_table"),
       tags$hr(),
       tags$br(),
       tags$h3("SNP Type and Diversity Statistics"),
+      HTML("<h5 style=color:grey>
+               This table provides basic statistics on the polymorphisms present in the given gene.
+               <br/>the columns \"5_prime_UTR_variant\" through \"coding_total\" are total, non unique numbers of variants, \"coding_total\" is the sum of missense and synonymous variants.
+               <br/>the final four columns are Nucleotide Diversity values, for different sections and SNP types
+            </h5>"),
       downloadButton("tab1.downloadStats","Download Content of Table Below"),
       tableOutput("SNPStats_Table")
     ),
 
     tabPanel("Diversity Plot",
-        ## Tab 2
+        ## Tab 2 ###############################################################
       textInput(inputId = "plotGene", label = "Type a single gene locus in the box below",
                 value = "AT1G80490"),
       actionButton(inputId="tab2.Submit", label = "Submit"),
       tags$hr(),
       tags$br(),
+      tags$h3("Selected Gene Information"),
       tableOutput("tab2.GeneInfo"),
-      h3("Plot of Nucleotide Diversity Statistic by Codon"),
-      h5("click and drag a box accross the plot below to see details on specific points"),
+      tags$hr(),
+      tags$br(),
+      tags$h3("Plot of Nucleotide Diversity Statistic by Codon"),
+      tags$h5("click and drag a box accross the plot below to see details on specific points"),
       plotOutput("diversityPlot", brush="plot_brush", click="plot_click", height = 400),
       verbatimTextOutput("info"),
       tags$hr(),
       tags$br(),
       tags$h3("Diversity Plot Data"),
+      tags$h5("This table provides the raw data from the plot. \"POS\" is the chromosomal position of the SNP, 
+              the Amino_Acid_Change field provides both the amino acid as well as the base change"),
       downloadButton("tab2.downloadSNPData","Download Content of Table Below"),
       DT::dataTableOutput("Diversity_Table")
     ),
     
     tabPanel("SNP Mapping",
-             ## Tab 3
+             ## Tab 3 ##########################################################
              textInput(inputId="tab3.Gene", label="Type a single gene locus in the box below",
                        value="AT1G80490"),
              actionButton(inputId="tab3.Submit", label="Submit"),
@@ -69,7 +80,8 @@ ui <- fluidPage(
              tags$hr(),
              tags$br(),
              tags$h3("Accession Map"),
-             tags$h5("Zoom with scroll wheel, click and drag to pan, click on individual point to see details"),
+             tags$h5("Zoom with scroll wheel, click and drag to pan. click on individual point to see details. 
+                     use the layers pop out to the lower left of the map to hide or show sets of markers with similar mutations"),
              leafletOutput("tab3.map", height="650"),
              tags$hr(),
              tags$br(),
@@ -78,45 +90,42 @@ ui <- fluidPage(
              #tableOutput("tab3Table")
              
              DT::dataTableOutput("tab3.dataTable")
-    ),
-    
-    tabPanel("Accessions and Mutations",
-             ## Tab 4
-             textInput(inputId="tab4.Gene", label="Type list of gene ID's",
-                       value="AT1G80490"),
-             actionButton(inputId="tab4.Submit", label="Get Data"),
-             
-             selectInput(inputId="tab4.filter_type", label="select an option",
-                         choices=c("list accessions by mutation", "list mutations by accession")),
-             
-             uiOutput("tab4.ui2"),
-             
-             fluidRow(
-               column(6, 
-                      h3("Accession Filter"),
-                      textAreaInput(inputId = "tab4.ecoIDs", label = "Ecotype ID(s)",
-                                    width = 400, height = 250, value = "" )
-                      ),
-               column(6,
-                      h3("Mutation Filter"),
-                      textAreaInput(inputId = "tab4.SNPs", label = "SNP(s)",
-                                    width = 400, height = 250, value = "" )
-                      )
-             ),
-             
-             actionButton(inputId="tab4.reFilter", label="Submit"),
-             
-             #tableOutput("tab4Table")
-             DT::dataTableOutput("tab4.dataTable")
-             
-             
-             
     )
     
+    # tabPanel("Accessions and Mutations",
+    #          ## Tab 4
+    #          textInput(inputId="tab4.Gene", label="Type list of gene ID's",
+    #                    value="AT1G80490"),
+    #          actionButton(inputId="tab4.Submit", label="Get Data"),
+    #          
+    #          selectInput(inputId="tab4.filter_type", label="select an option",
+    #                      choices=c("list accessions by mutation", "list mutations by accession")),
+    #          
+    #          uiOutput("tab4.ui2"),
+    #          
+    #          fluidRow(
+    #            column(6, 
+    #                   h3("Accession Filter"),
+    #                   textAreaInput(inputId = "tab4.ecoIDs", label = "Ecotype ID(s)",
+    #                                 width = 400, height = 250, value = "" )
+    #                   ),
+    #            column(6,
+    #                   h3("Mutation Filter"),
+    #                   textAreaInput(inputId = "tab4.SNPs", label = "SNP(s)",
+    #                                 width = 400, height = 250, value = "" )
+    #                   )
+    #          ),
+    #          
+    #          actionButton(inputId="tab4.reFilter", label="Submit"),
+    #          
+    #          #tableOutput("tab4Table")
+    #          DT::dataTableOutput("tab4.dataTable")
+    #)
     
-  ),
+    
+  )
 
-  "THIS IS THE FOOTER"
+  # "THIS IS THE FOOTER"
 )
 #=================================================================
 
@@ -139,17 +148,6 @@ load_tab_2_Data <- function (geneInfo){
   return(coding_variants)
 }
 
-parsebysep <- function (textIn, sep) {
-  text <- gsub(" ", "", ttext, fixed = TRUE)
-  output <- strsplit(text, sep)
-  
-  
-}
-
-
-#writeData <- function (table, file){
-#  write.csv(tableData, file)
-#}
 
 plotPi <- function(unique_coding_variants) {
   plot <- ggplot(unique_coding_variants, aes(x=Codon_Number,y=Diversity, colour=Effect)) +
@@ -158,10 +156,8 @@ plotPi <- function(unique_coding_variants) {
     #scale_colour_manual(values=c(synonymous_diversity="blue", missense_diversity="red")) +
     ylab("nucleotide diversity, log scale")
   return(plot)
-  
+
 }
-
-
 
 
 
@@ -178,13 +174,14 @@ server <- function(input, output){
   ## Tab 1 stuff:
 
   tab1.Genes <- eventReactive(input$STATS_submit,{
+    # list of genes for tab 1, updated on pressing submit button
     names <- parseInput(input$gene_ids)
     genes <- getGeneInfo(names)
     req(genes != FALSE)
     return(genes)
   })
   
-  output$table <- renderTable(tab1.Genes())
+  output$tab1.genes_table <- renderTable(tab1.Genes())
     
   SNPStats <- reactive({polymorphTable(tab1.Genes(), strains)})
   
@@ -255,7 +252,7 @@ server <- function(input, output){
   
   
   tab3.Genes <- eventReactive(input$tab3.Submit, {
-    #gene Info for gene on tab 2, updates on 'submit' button press
+    #gene Info for gene on tab 3, updates on 'submit' button press
     names <- parseInput(input$tab3.Gene)
     genes <- getGeneInfo(names[1])
     return(genes)
@@ -263,6 +260,8 @@ server <- function(input, output){
   
   
   tab3.tidyData <- reactive({
+    
+    # Get the data
     tidyVCF <- VCFByTranscript(tab3.Genes()[1, ], strains)
     data <- tidyVCF$dat
     # Parse the EFF field
@@ -278,34 +277,38 @@ server <- function(input, output){
   })
   
   tab3.EffectValues <- reactive({    
-    effects <- c("5_prime_UTR_variant",
-                 "intron_variant",
-                 "3_prime_UTR_variant",
-                 "synonymous_variant",
-                 "missense_variant",
-                 "upstream_gene_variant",
-                 "downstream_gene_variant")
+    # effects <- c("5_prime_UTR_variant",
+    #              "intron_variant",
+    #              "3_prime_UTR_variant",
+    #              "synonymous_variant",
+    #              "missense_variant",
+    #              "upstream_gene_variant",
+    #              "downstream_gene_variant")
     
     effects <- unique(tab3.tidyData()$Effect)
-    switch(input$tab3.SNPtype, 
-           "All"=effects,
-           "Missense"="missense_variant",
-           "Coding"= c("missense_variant", "synonymous_variant"))
-    #return(input$tab3.filter_value)
+
+    return( switch(input$tab3.SNPtype, 
+            "All"=effects,
+            "Missense"="missense_variant",
+            "Coding"= c("missense_variant", "synonymous_variant"))
+    )
   })
   
   output$tab3.debug <- renderPrint({
+    # temporary debug output
     input$tab3.mutation_select
   })
   
   tab3.filteredByDiv <- reactive({
     # filter by diversity slider and SNP type radio button then add SNPs column
+    
     data <- tab3.tidyData()
     
+    # filter by effect type (all, coding, or missense)
     data2 <- data[data$Effect %in% tab3.EffectValues(), ]
     
+    # filter on positions with diversity greater than or equal to the 10^slider value
     keyPOS <- unique(data2[which(data2$Diversity >= 10**input$tab3.filter_value), "POS"])
-    
     keydata <- data[data$POS %in% keyPOS, ]
     
     return(keydata)
@@ -320,7 +323,9 @@ server <- function(input, output){
   output$tab3.mutation_checkbox <- renderUI({
     tagList(
       tags$hr(),
-      tags$br(),      
+      tags$br(),     
+      tags$h3("Mutation select"),
+      tags$h5("Select the SNPs you want to see on the map by clicking the checkboxes"),
       tags$div(class="checkboxformat", 
                checkboxGroupInput("tab3.mutation_select", "select_mutations to display", choices=tab3.mutationList())
       ),
@@ -330,9 +335,13 @@ server <- function(input, output){
   })
     
   tab3.labeled <- eventReactive(input$tab3.update_map, {
-    data <- tab3.filteredByDiv()
+    # a dataframe with a single row per accession, containing accession info, 
     
+    # start with the data filtered by the diversity slider and type buttons
+    data <- tab3.filteredByDiv()
+    # label by SNPs creates column SNPs with text strings formatted [transcriptID|AA_Change]
     data <- label_bySNPs(data, collapse=FALSE)
+    # filter on selected SNPs
     data <- data[data$SNPs %in% input$tab3.mutation_select, ]
     # combine mutations to single row (this is slow)
     data <- ddply(data, "Indiv", summarise, SNPs=paste(SNPs, collapse=","))
@@ -340,7 +349,6 @@ server <- function(input, output){
     data <- add_ecotype_details(data)
     
     return(data)
-  
   })
   
   output$tab3.map <- renderLeaflet({
@@ -353,41 +361,40 @@ server <- function(input, output){
     # make a field with text to be displayed when clicking on a marker
     mapdata$popup <- paste("EcoID:",  mapdata$Indiv,"Name:", mapdata$Name, " SNPs:", mapdata$SNPs)
     
-    #pal <- c(brewer.pal(8, "Set1"), brewer.pal(7, "Dark2"))
+    # create the color pallet for the map points
     pal <- brewer.pal(8, "Set1")
     pallet <- colorFactor(palette=pal, domain=mapdata$SNPs)
     
+    # create a new leaflet map
     map <- leaflet()
-    
     map <- addProviderTiles(map, providers$Stamen.TonerLite,
                      options = providerTileOptions(noWrap = TRUE))
     
+    # groupnames to be used by draw groups of points as separate layers below
     groupnames <- unique(mapdata$SNPs)
     groupnames <- groupnames[!is.na(groupnames)]
     
+    # add markers for NA points first so they are furthest back layer
     map <- addCircleMarkers(map, data=mapdata[is.na(mapdata$SNPs), ], color= "#9b9b9b", group="NA", 
                             radius=6, popup= ~popup, stroke=FALSE, fillOpacity=0.6)
-    
+   
+    # for each of the group names, add a set of markers  
     for (SNP in groupnames){
           map <- addCircleMarkers(map, data=mapdata[mapdata$SNPs == SNP, ], color= ~pallet(SNPs), group= SNP, 
                             radius=6, popup= ~popup, stroke=FALSE, fillOpacity=0.85)
     }
 
-
-    
+    # add the legend to the map
     map <- addLegend(map, position="bottomright", pal=pallet, 
                      values=mapdata$SNPs, title="Marker Colors", opacity=1)
     
+    # add layer control to map to turn on or off groups of points
     map <- addLayersControl(map, overlayGroups=c(groupnames, "NA"), 
                             options = layersControlOptions(collapsed = TRUE),
                             position="bottomleft")
     
-
     return(map)
-
   })
-  
-  output$tab3Table <- renderTable(tab3.labeledSNPs()[1:350, ], digits=5)
   
   output$tab3.dataTable <- DT::renderDataTable(tab3.labeled())
 
@@ -407,53 +414,50 @@ server <- function(input, output){
   ## --------------------------------------           ----------------
   ## Tab 4 stuff:
   
-  tab4.Genes <- eventReactive(input$tab4.Submit, {
-    #gene Info for gene on tab 2, updates on 'submit' button press
-    names <- parseInput(input$tab4.Gene)
-    genes <- getGeneInfo(names[1])
-    return(genes)
-  })
-  
-  tab4.tidyData <- reactive({
-    tidyVCF <- VCFByTranscript(tab4.Genes()[1, ], strains)
-    data <- tidyVCF$dat[tidyVCF$dat$gt_GT != "0|0",]
-    # Parse the EFF field
-    data <- parseEFF(tidyVCF = data, Transcript_ID = tab4.Genes()$transcript_ID[1])
-    
-    # calculate diversity
-    data <- Nucleotide_diversity(data)
-    data <- add_ecotype_details(data)
-    data <- subset(data, select=-c(EFF))
-    
-    
-    return(data)
-  })
-  
-  output$tab4.ui2 <- renderUI({
-    
-    if (input$tab4.submit==0) {
-      return(tags$h4("mot submitted"))
-    }
-    
-    return(tags$h4("submitted"))
-    
-    
-    
-  })
-  
-  
-  tab4.filteredData <- eventReactive(input$tab4.reFilter, {
-    data <- tab4.tidyData
-    
-    if (input$tab4.ecoIDs != ""){
-      
-    }
-    
-    
-  })
-  
-  
-  output$tab4.dataTable <- DT::renderDataTable(DT::datatable(tab4.tidyData()))
+  # tab4.Genes <- eventReactive(input$tab4.Submit, {
+  #   #gene Info for gene on tab 2, updates on 'submit' button press
+  #   names <- parseInput(input$tab4.Gene)
+  #   genes <- getGeneInfo(names[1])
+  #   return(genes)
+  # })
+  # 
+  # tab4.tidyData <- reactive({
+  #   tidyVCF <- VCFByTranscript(tab4.Genes()[1, ], strains)
+  #   data <- tidyVCF$dat[tidyVCF$dat$gt_GT != "0|0",]
+  #   # Parse the EFF field
+  #   data <- parseEFF(tidyVCF = data, Transcript_ID = tab4.Genes()$transcript_ID[1])
+  #   
+  #   # calculate diversity
+  #   data <- Nucleotide_diversity(data)
+  #   data <- add_ecotype_details(data)
+  #   data <- subset(data, select=-c(EFF))
+  #   
+  #   
+  #   return(data)
+  # })
+  # 
+  # output$tab4.ui2 <- renderUI({
+  #   
+  #   if (input$tab4.submit==0) {
+  #     return(tags$h4("mot submitted"))
+  #   }
+  #   
+  #   return(tags$h4("submitted"))
+  #   
+  #   
+  #   
+  # })
+  # 
+  # tab4.filteredData <- eventReactive(input$tab4.reFilter, {
+  #   data <- tab4.tidyData
+  #   
+  #   if (input$tab4.ecoIDs != ""){
+  #   }
+  #   
+  #   
+  # })
+  # 
+  # output$tab4.dataTable <- DT::renderDataTable(DT::datatable(tab4.tidyData()))
   
   
 }
