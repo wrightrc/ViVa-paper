07910ed454a27dde0b4c1f315edf4b50ff0d3117
Clay Wright <wright.clay@gmail.com>
2018-05-22 13:23:13 -0700

    Added IAA tree figure

--- manuscript/readSelection.py
+++ manuscript/readSelection.py
@@ -1,4 +1,4 @@
-from pymol import cmd
+from pymol import cmd, stored
 
 def readSelection (fileName):
 	'''
@@ -7,17 +7,16 @@ def readSelection (fileName):
 	reads a csv file containing a column of selection names and a column of
 	selection strings and creates selection objects for each
 	'''
-	line = []
 	#read in the selection file
 	with open(fileName) as f:
-		for line in f:
+		lines = f.readlines()
+		for line in lines:
 			sel = line.split(",")
-			print sel[0] + " " + sel[1]
-			#cmd.select(selection[0], selection[1])
+			cmd.select(sel[0], sel[1])
 
 	f.close()
 
-cmd.extend('readSelection',readSelection)
+cmd.extend('readSelection',readSelection);
 
 
 
